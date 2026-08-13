944f55eb17af5e332d59ce882e8024299f05706a
Evhenii Terekhov <johnlarkin579@gmail.com>
2026-08-13 14:32:04 +0300

    Add Folder and FolderTD files

---
+++ Folder.java
@@ -0,0 +1,45 @@
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+
+public class Folder {
+    private static final Folder INSTANCE = new Folder();
+    private String path = Path.of("Task-Manager/ListOfTasks" ).toString()+ File.separator;
+
+    private Folder() {}
+
+    public static Folder getInstance() {
+        return INSTANCE;
+    }
+
+    public String getPath() {
+        return path;
+    }
+
+    /**
+     * Создает файл с именем title с расширением .txt
+     * @param title задает имя файла
+     * @throws IOException если файл уже существует
+     */
+    public void createFile(String title) {
+        try {
+            Files.createFile(Path.of(getInstance().getPath() + title + ".txt"));
+        } catch (IOException e) {
+            System.out.println("File exists");
+        }
+    }
+
+    /**
+     * Удаляет файл с именем - title с расширением .txt
+     * @param title задет имя файла
+     * @throws IOException если файл не существует
+     */
+    public void deleteFile(String title) {
+        try {
+            Files.delete(Path.of(getPath() + title + ".txt"));
+        } catch (IOException e) {
+            System.out.println("File not exists");
+        }
+    }
+}
